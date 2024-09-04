fdc5cdaa2c733f6ce35bf9e2a1176b18356e4fc2
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-09-04 22:46:50 +0600

    test route added

---
+++ emp-frontend/app/index.jsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 22,
+
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
