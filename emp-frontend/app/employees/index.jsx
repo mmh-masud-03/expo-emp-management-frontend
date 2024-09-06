1e0dad13f70ac4ddfb139574d9394dc154c7c739
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-09-06 15:29:21 +0600

    loader added

--- emp-frontend/app/employees/index.jsx
+++ emp-frontend/app/employees/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import { useRouter } from "expo-router";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -8,6 +8,7 @@ import { fetchEmployees, deleteEmployee } from "../../services/api";
 
 const EmployeesPage = () => {
   const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true); // Loader state
   const router = useRouter();
 
   useEffect(() => {
@@ -15,9 +16,11 @@ const EmployeesPage = () => {
   }, []);
 
   const loadEmployees = async () => {
+    setLoading(true); // Set loading to true before fetching
     const data = await fetchEmployees();
     const employees = data?.employees;
     setEmployees(employees || []);
+    setLoading(false); // Set loading to false after fetching
   };
 
   const handleDelete = async (id) => {
@@ -29,7 +32,9 @@ const EmployeesPage = () => {
     <View className="flex-1">
       <Header title="Employees" showAddButton />
       <View className="flex-1">
-        {employees.length > 0 ? (
+        {loading ? (
+          <ActivityIndicator size="large" className="mt-4" /> // Loader component
+        ) : employees.length > 0 ? (
           <EmployeeTable employees={employees} onDelete={handleDelete} />
         ) : (
           <Text className="text-center mt-4">No employees found</Text>
